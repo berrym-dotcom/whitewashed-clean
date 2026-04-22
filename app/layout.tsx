e1856e20ba8b6d2fc0a0cfc26b28c50a86fde4df
berrym-dotcom <mfksfo@gmail.com>
2026-04-21 17:55:10 -0700

    Add RootLayout component for application layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,22 @@
 
+export default function RootLayout({
+
+  children,
+
+}: {
+
+  children: React.ReactNode;
+
+}) {
+
+  return (
+
+    <html>
+
+      <body>{children}</body>
+
+    </html>
+
+  );
+
+}
